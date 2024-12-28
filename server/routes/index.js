4d1691f2aabc9ea823569ba46b99f13e7454d4b5
Harikrishnan K <harikdas2001@gmail.com>
2024-12-29 01:22:56 +0530

    / api

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,6 +6,10 @@ const auth = require('./authRoute.js');
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send('Hello, Lockspot!');
+});
+
 app.use('/auth', auth);
 
 module.exports = app;
