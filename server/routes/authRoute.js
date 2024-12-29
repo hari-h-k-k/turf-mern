9b3ca96426288da661a19446dd66f4035daee9c5
Harikrishnan K <harikdas2001@gmail.com>
2024-12-29 10:24:23 +0530

    build commit

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-const authController=require('../db/controllers/authController.js.js');
+const authController=require('../db/controllers/authController.js');
 
 router.get('/', (req, res) => {
     const items = [
